73ef8a3d7ef378451e9b183786858f78263ead77
shenfive <shennewno@gmail.com>
2017-04-02 16:17:07 +0800

    NLP Free Android
    init

---
+++ app/src/main/java/idv/sjw/nlpfree/MainActivity.java
@@ -0,0 +1,17 @@
+package idv.sjw.nlpfree;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide(); //隱藏標題
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION); //隱藏狀態
+    }
+}
